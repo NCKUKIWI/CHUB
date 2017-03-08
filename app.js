4413ebb1553f8233162aec0b3c3a413a27489749
BoTung <bver49@gmail.com>
2017-03-08 20:24:47 +0800

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var engine = require('ejs-locals');             //讓express支援layout
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+
+app.engine('ejs', engine);
+app.set('views',path.join(__dirname,'views'));  //view的路徑位在資料夾views中
+app.set('view engine','ejs');                   //使用ejs作為template
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Validator
+app.use(expressValidator());
+app.use("/assets",express.static(__dirname + "/assets"));
+
+//Handle sessions and cookie
+app.use(session({
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },
+  secret:'secret',
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(cookieParser('secretString'));
+
+app.listen( process.env.PORT || 3000);                             //監聽3000port
+console.log('running on port 3000');
