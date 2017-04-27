2769dc4cf0e17dba72b450cbe90442c7ab5803ea
BoTung <bver49@gmail.com>
2017-04-28 01:03:36 +0800

    change file structure

---
+++ controller/users.js
@@ -0,0 +1,181 @@
+var express = require("express");
+var router = express.Router();
+var User = require("../model/User");
+var Message = require("../model/Message");
+var config = require("../config");
+var helper = require("../helper");
+var graph = require("fbgraph");
+
+router.get("/", function(req,res) {
+  var query = {
+    Talent:req.query.talent,
+    Major:req.query.major
+  }
+  var filter = {
+    $or:[]
+  };
+  for(var i in query){
+    if(query[i]!== undefined){
+      filter["$or"].push({i:query[i]});
+    }
+  }
+  if(filter["$or"].length == 0) filter["$or"].push({});
+  User.find(filter,["_id","Email","Major","Talent","Description","Website","Role"],function(err,users){
+    res.render("users/index",{
+      me:req.user,
+      users:users
+    });
+  });
+});
+
+router.get("/login", function(req,res) {
+  res.render("users/login",{
+    me:req.user
+  });
+});
+
+router.get("/signup", function(req,res) {
+  res.render("users/signup",{
+    me:req.user
+  });
+});
+
+router.post("/signup", function(req,res) {
+  var newUser = {
+    UserID:req.body.userid,
+    Password:req.body.password,
+    Name:req.body.username,
+    Email:req.body.email,
+    Role:0,
+  };
+  User.create(newUser,function(err,result){
+    if(err){
+      res.send({error:helper.handleError(err)});
+    }else{
+      //helper.sendEmail(result.Email,"驗證信",`您好請點擊以下連結開通\n\nhttp://localhost/user/emailauth?user=${result.UserID}&id=${result._id}`);
+      res.send("ok");
+    }
+  });
+});
+
+router.post("/auth", function(req, res) {
+  User.findOne({UserID:req.body.userid},["UserID","Password"],function(err,user){
+    if(user){
+      if(user.Password===req.body.password){
+        res.cookie("isLogin",1,{maxAge: 60 * 60 * 1000});
+        res.cookie("id", user._id,{maxAge: 60 * 60 * 1000});
+        res.send("ok");
+      }else{
+        res.send({error:"pwError"});
+      }
+    }else{
+      res.send({error:"notFound"});
+    }
+  });
+});
+
+router.get("/fblogin", function(req, res) {
+  res.redirect(`https://www.facebook.com/v2.8/dialog/oauth?client_id=${config.fb_id}&scope=email,public_profile&response_type=token&redirect_uri=http://localhost:3000/user/fbcheck`);
+});
+
+router.get("/fbcheck", function(req,res) {
+  if(req.query.access_token){
+    graph.get(`/me?fields=id,name,email,gender&access_token=${req.query.access_token}`,function(err,fb){
+      User.findOne({UserID:fb.id},"_id",function(err,user){
+        if(user){
+          res.cookie("isLogin",1,{maxAge: 60 * 60 * 1000});
+          res.cookie("id",user._id,{maxAge: 60 * 60 * 1000});
+          res.redirect("/");
+        }
+        else{
+          User.create({ UserID:fb.id,Name:fb.name,Password:fb.id,Role:0}, function (err,result) {
+            if (err) console.log(err);
+            helper.sendEmail(result.Email,"驗證信",`您好請點擊以下連結開通\n\nhttp://localhost/user/emailauth?user=${result.UserID}&id=${result._id}`);
+            res.cookie("isLogin",1,{maxAge: 60 * 60 * 1000});
+            res.cookie("id",result._id,{maxAge: 60 * 60 * 1000});
+            res.redirect("/");
+          })
+        }
+      });
+    });
+  }else{
+    res.render("users/fblogin");
+  }
+});
+
+router.get("/emailauth", function(req, res){
+  if(req.query.user && req.query.id){
+    User.findOne({_id:req.query.id,UserID:req.query.user},["_id","Role"],function(err,user){
+      if(user){
+        user.Role = 1;
+        user.save(function(err){
+          if(err){
+            res.send({error:helper.handleError(err)});
+          }else{
+            res.send("開通成功");
+          }
+        });
+      }else{
+        res.redirect("/");
+      }
+    });
+  }else{
+    res.redirect("/");
+  }
+});
+
+router.post("/update",helper.checkLogin(),function(req, res) {
+  var newData = {
+    UserID:req.body.userid,
+    Email:req.body.email,
+    Name:req.body.name,
+    Major:req.body.major,
+    Talent:req.body.talent.split(","),
+    Description:req.body.description,
+    Website:req.body.website
+  }
+  User.findOneAndUpdate({_id:req.user._id},newData,function(err,user){
+    if(err){
+      res.send({error:helper.handleError(err)});
+    }else{
+      res.send("ok");
+    }
+  });
+});
+
+router.get("/logout", function(req, res) {
+  res.clearCookie("isLogin");
+  res.clearCookie("id");
+  res.redirect("/");
+});
+
+router.get("/msg",helper.checkLogin(),function(req,res) {
+  Message.find({ToUID:req.user._id}).populate("FromUID").populate("FromGID").exec(function(err,msg){
+    res.send({
+      me:req.user,
+      msg:msg
+    });
+  });
+});
+
+router.post("/loginStatus", function(req,res) {
+  if(req.user){
+    res.send({
+      me:req.user
+    });
+  }else{
+    res.send({error:"notLogin"});
+  }
+});
+
+router.get("/:id", function(req,res) {
+  User.findOne({UserID:req.params.id},["_id","Email","Major","Talent","Description","Website","Role"],function(err,user){
+    res.render("users/show",{
+      me:req.user,
+      user:user
+    });
+  });
+});
+
+
+module.exports = router;
--- src/client/components/what_on.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <div class="head">
-      <h1>What's on</h1>
-      <h2>C-Hub is a creative space came about from the interconnection among Creativity, Human, Urban, and Buidling Environment.</h2>
-    </div>
-  </div>
-</template>
