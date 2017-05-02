e6fe89e814913776dc4a57d96b4013b6f62827ee
BoTung <bver49@gmail.com>
2017-05-02 11:32:54 +0800

    edit gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,24 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 var nodemon = require('gulp-nodemon');
-var gulpSass = require('gulp-sass');
-var browserSync = require('browser-sync');
 
-gulp.task('default', ['browser-sync'], function () {
-});
-
-gulp.task('browser-sync', ['nodemon'], function() {
+gulp.task('browser-sync', ['sass','nodemon'], function () {
 	browserSync.init(null, {
 		proxy: "http://localhost:3000",
-      files: ["views/*.*","views/**/*.*","assets/css/*.*","assets/js/*.*"],
-      browser: "google chrome",
-      port:7000,
+		port:7000
 	});
 });
 
-gulp.task('styles', function () {
-    gulp.src('scss/**/*.scss')
-        .pipe(gulpSass())
-        .pipe(gulp.dest('css'));  
+// Compile SASS檔案
+gulp.task('sass', function() {
+    return gulp.src("assets/scss/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("assets/css"))
+      .pipe(browserSync.stream());
 });
 
+//啟動server
 gulp.task('nodemon', function (cb) {
 	var started = false;
 	return nodemon({
@@ -32,3 +30,14 @@ gulp.task('nodemon', function (cb) {
 		}
 	});
 });
+
+//監看 檔案有改變時刷新或compile
+gulp.task('watch', function () {
+  gulp.watch("app/scss/*.scss", ['sass']);
+	gulp.watch("assets/css/*.*").on('change', browserSync.reload);
+	gulp.watch("assets/js/*.*").on('change', browserSync.reload);
+	gulp.watch("views/*.*").on('change', browserSync.reload);
+  gulp.watch("views/**/*.*").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
