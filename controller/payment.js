b2fd99e4775660f210972c0de4d52f0d412b7335
BoTung <bver49@gmail.com>
2017-05-07 11:41:27 +0800

    fix payment

--- controller/payment.js
+++ controller/payment.js
@@ -8,16 +8,11 @@ var sha256 = require("sha256");
 
 router.get("/", function(req,res) {
   var timestamp = Date.now();
-  //var check =`HashKey=${ config.pay2go.hashkey }&Amt=2000&MerchantID=PG100000002050&MerchantOrderNo=1212389&TimeStamp=${ timestamp }&Version=1.0&HashIV=${ config.pay2go.hashiv }`;
-  //check = sha256(check).toUpperCase();
-  var info = `MerchantID=PG100000002050&TimeStamp=${ timestamp }&Version=1.0&MerchantOrderNo=1212389&Amt=2000&ItemDesc=someword`;
-  var tradeinfo = helper.encrypt(config.pay2go.hashkey,config.pay2go.hashiv,info).trim();
-  var tradesha = `HashKey=${ config.pay2go.hashkey }&${ tradeinfo }&HashIV=${ config.pay2go.hashiv }`;
-  tradesha = sha256(tradesha).toUpperCase();
+  var check =`HashKey=${ config.pay2go.hashkey }&Amt=2000&MerchantID=MS11571737&MerchantOrderNo=1212389&TimeStamp=${ timestamp }&Version=1.2&HashIV=${ config.pay2go.hashiv }`;
+  check = sha256(check).toUpperCase();
   res.render("payment",{
     timestamp:timestamp,
-    tradeinfo:tradeinfo,
-    tradesha:tradesha
+    check:check
   });
 });
 
