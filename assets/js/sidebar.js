aad6a692cfd9ca7f5764e4b2f635bf0ed55af08f
davinlin <davin135@gmail.com>
2017-05-09 20:29:59 +0800

    complete message bar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -60,4 +60,26 @@ $(document).ready(function() {
 		})
 		$('#backProfile').trigger( "click" );
 	})
+	$('#msgSend').on('click', function(){
+		sendMessage();
+	})
+	$(document).keypress(function(e) {
+		if(e.which == 13) {
+			sendMessage();
+		}
+	});
 });
+
+
+function sendMessage(){
+	var sendMsg = $('#msgText').val();
+	$(".chatCont").append('<li class="chatEntry chatSent"><img class="avatar" src="//placekitten.com/56/56" /><p class="message">'+sendMsg+'<time class="timestamp">4 minutes ago</time></p></li>');
+	$('#msgText').val('');
+}
+
+var receiveText = "testtt";
+
+function getMessage(receiveText){
+	var getMsg = receiveText;
+	$(".chatCont").append('<li class="chatEntry"><img class="avatar" src="//placekitten.com/g/50/50" /><p class="message">'+getMsg+'<time class="timestamp">4 minutes ago</time></p></li>');
+}
