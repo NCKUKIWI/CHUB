1b81e177f255e53b5f7327a3c0800c396504de5e
BoTung <bver49@gmail.com>
2017-05-13 15:03:42 +0800

    add group admin in panel

--- controller/panel.js
+++ controller/panel.js
@@ -12,9 +12,17 @@ router.get("/", function(req,res) {
 });
 
 router.get("/projects",function(req,res) {
-  Project.find({"MemberID":{"$in":[req.user._id]}},function(err,projects){
+  Project.find({"AdminID":{"$in":[req.user._id]}},function(err,projects){
     res.render("panel/project",{
       projects:projects
+    });
+  });
+});
+
+router.get("/groups",function(req,res) {
+  Group.find({"AdminID":{"$in":[req.user._id]}},function(err,groups){
+    res.render("panel/group",{
+      groups:groups
     });
   });
 });
