a727eed9f08c4967846f464530c58348704b48ba
BoTung <bver49@gmail.com>
2017-05-19 17:03:08 +0800

    add model save payment record

--- controller/payment.js
+++ controller/payment.js
@@ -3,16 +3,31 @@ var router = express.Router();
 var helper = require("../helper");
 var config = require("../config");
 var User = require("../model/User");
+var Payment = require("../model/Payment");
 var Activity = require("../model/Activity");
-var sha256 = require("sha256");
 
-router.get("/return", function(req,res) {
+router.post("/return", function(req,res) {
   res.send("付款成功");
 });
 
 router.post("/notify", function(req,res) {
-  console.log(req.body);
-  res.send("ok");
+  var data = req.body.JSONData.Result;
+  var newPayment = new Payment({
+    Amt:data.Amt,
+    TradeNo:data.TradeNo,
+    MerchantOrderNo:data.MerchantOrderNo,
+    PayTime:data.PayTime,
+    PaymentType:data.PaymentType,
+    EscrowBank:data.EscrowBank
+  });
+  newProject.save(function(err){
+    if(err){
+      console.log(err);
+      res.send({error:helper.handleError(err)});
+    }else{
+      res.send("ok");
+    }
+  });
 });
 
 module.exports = router;
---
+++ model/Payment.js
@@ -0,0 +1,44 @@
+var mongoose = require("./mongoose");
+var uniqueValidator = require("mongoose-unique-validator");
+var Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
+/*
+Schema Type
+------------
+What? => In mongoose
+Words => String
+True/False => Boolean
+Integer => Number
+Array => []
+Array of String => [String]
+
+Mutiple option
+{type: Date , default: Date.now }
+{type: String , unique: true, required: true }
+{type: String , minlength: 18, maxlength: 65 }
+*/
+
+var paymentSchema = new Schema({
+  Amt:Number,
+  TradeNo:String,
+  MerchantOrderNo:String,
+  PayTime:String,
+  PaymentType:String,
+  EscrowBank:String,
+  CreateAt: { type: Date, default: Date.now }
+});
+
+paymentSchema.pre("find", function(next) {
+  this.start = Date.now();
+  next();
+});
+
+paymentSchema.post("find", function(result) {
+  //console.log(JSON.stringify(result,null,4));
+  console.log(`Took ${ Date.now() - this.start} millis`);
+});
+
+paymentSchema.plugin(uniqueValidator);
+var Payment = mongoose.model("Payment", paymentSchema);
+
+module.exports = Payment;
