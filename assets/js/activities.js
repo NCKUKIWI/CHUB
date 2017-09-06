083e3a4bc5824d49da2c2beb297eae294d00a1d8
nick0916 <nickliao2013@gmail.com>
2017-09-06 14:32:44 +0800

    fix activity bug

--- assets/js/activities.js
+++ assets/js/activities.js
@@ -68,7 +68,7 @@ $(document).ready(function(){
                 url: "/activities/" + id,
                 type: "POST",
                 success: function(response) {
-                    $.fn.fullpage.setAllowScrolling(false, "down,up"); // 停止第一層的fullpage滑動
+                    // $.fn.fullpage.setAllowScrolling(false, "down,up"); // 停止第一層的fullpage滑動
                     $(".float_window").append(response);
                     $("#close_window, .dark_mask").click(function() {
                         close_window();
