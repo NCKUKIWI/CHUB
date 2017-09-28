a051b43ef33fb387daab71240e9b8ab136d81fe6
davinlin <davin135@gmail.com>
2017-09-29 05:45:59 +0800

    check browser

---
+++ assets/js/check_browser.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){ 
+    var client = new ClientJS();
+    var isIE = client.isIE(); // Check For IE
+    var isFirefox = client.isFirefox(); // Check For Firefox
+    var isOpera = client.isOpera(); // Check For Opera
+    var isWindows = client.isWindows(); // Check For Mac
+    
+    if ( isIE || isOpera ) {
+        noSupport();
+    }
+    
+    if ( isFirefox && isWindows ) {
+        noSupport();
+    }
+});
+
+
+function noSupport () {
+    location.href="http://google.com";
+    // location.href="no_support.html";
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  
   /* toastr option */
   toastr.options = {
     "closeButton": true,
